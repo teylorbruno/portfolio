df2b6e0f03febb9bd8506f818fd5dc611e812dd8
bruno domingues <teylorbruno@gmail.com>
2021-12-30 16:10:11 +0000

    experimento

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,6 +5,16 @@ const app = {
         const rand = Math.floor(Math.random() * 2);
         const source = utils._videoPath + rand + utils._ext;
         videoEl.src = source;
+    })(),
+    aClick: (function () {
+        window.addEventListener('click', event => {
+            if (event.target.href !== undefined && event.target.href !== '') {
+                const main = document.getElementById('main');
+                const header = document.getElementById('header');
+                main.style.width = '100%';
+                header.style.display = 'none';
+            }
+        });
     })()
 }
 
